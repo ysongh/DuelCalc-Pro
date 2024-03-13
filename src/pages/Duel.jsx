fd4f52bbf5b2e111aafc52dde13160b3d7aa867a
You Song Hou <ysongweb3@gmail.com>
2024-03-12 22:14:38 -0400

    refactor: make the duel page responsive

--- src/pages/Duel.jsx
+++ src/pages/Duel.jsx
@@ -1,18 +1,18 @@
-import { Container, Flex, Text } from "@chakra-ui/react";
+import { Container, SimpleGrid, Text } from "@chakra-ui/react";
 import Calculator from "../components/Calculator";
 import Calculator2 from "../components/Calculator2";
 
 function Duel() {
   return (
-    <Container>
-      <Text fontSize="2xl" mb={4} textAlign="center">
+    <Container maxW='1000px'>
+       <Text fontSize="2xl" mb={4} textAlign="center">
         DuelCalc Pro
       </Text>
-      <Flex gap="2">
+      <SimpleGrid minChildWidth='250px' columns={3} spacing={4}>
         <Calculator name="Player 1"/>
         <Calculator name="Player 2" />
         <Calculator2 />
-      </Flex>
+      </SimpleGrid>
     </Container>
   )
 }
